a3cc392ebaef3d736e0037952c7f972cc1ef78ef
seml <4040591@gmail.com>
2023-10-06 10:32:53 +0300

    added draft step

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FirstStep from '@/views/FirstScreen.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'first-step',
+      component: FirstStep
+    }
+  ]
 })
 
 export default router
---
+++ src/views/FirstScreen.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>first</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'FirstStep'
+}
+</script>
